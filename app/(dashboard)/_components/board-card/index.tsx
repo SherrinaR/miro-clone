695925e74608150fc472d563581d8a53ab768028
Sherrina <robinsonsherrina@gmail.com>
2025-03-05 19:10:03 -0500

    added dropdown menu button to board cards

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
 import Image from "next/image";
+import { useAuth } from "@clerk/nextjs";
+import { MoreHorizontal } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
 
+import { Actions } from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { Overlay } from "./overlay";
-import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
-import { Actions } from "@/components/actions";
+
 
 interface BoardCardProps {
     id: string;
@@ -57,8 +59,15 @@ export const BoardCard = ({
                         title={title}
                         side="right"
                     >
-                        <button>
-                            I am a button
+                        <button
+                            className="absolute top-1 right-1 opacity-0
+                            group-hover:opacity-100 transition-opacity px-3 
+                            py-2 outline-none"
+                        >
+                            <MoreHorizontal
+                                className="text-white opacity-75
+                                hover:opacity-100 transition-opacity"
+                             />
                         </button>
                     </Actions>
                 </div>
