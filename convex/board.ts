92bb97c07329ac8720e0f3ac6adcebff11156ed6
Sherrina <robinsonsherrina@gmail.com>
2025-03-19 16:22:53 -0400

    created functionality to update boards

--- convex/board.ts
+++ convex/board.ts
@@ -37,7 +37,7 @@ export const create = mutation({
     },
 });
 
-/* the delete functionality for boards */
+/* the functionality to delete boards */
 export const remove = mutation({
     args: {id: v.id("boards") },
     handler: async (ctx, args) => {
@@ -47,8 +47,36 @@ export const remove = mutation({
             throw new Error("Unauthorized");
         }
 
-        // TODO: Check to delete favorit relation
+        // TODO: Check to delete favorite relation
 
         await ctx.db.delete(args.id);
+    },
+});
+
+/* the functionality to update boards */
+export const update = mutation({
+    args: {id: v.id("boards"), title: v.string() },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const title = args.title.trim();
+
+        if (!title) {
+            throw new Error("Title is required");
+        }
+
+        if (title.length > 60) {
+            throw new Error("Title cannot be longer than 60 characters")
+        }
+
+        const board = await ctx.db.patch(args.id, {
+            title: args.title,
+        });
+
+        return board;
     },
 });
