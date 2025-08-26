c7d0cf3c858e94b96d65cc42586bfde4966c48b8
Sherrina <robinsonsherrina@gmail.com>
2025-08-25 20:54:04 -0400

    created single board page

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,11 @@
+/* Board layout page without the dashboard and sidebar */
+
+import { Canvas } from "./_components/canvas";
+
+const BoardIdPage = () => {
+    return (
+        <Canvas />
+    );
+};
+
+export default BoardIdPage;
