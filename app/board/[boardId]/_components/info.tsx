f05344534b53fe1f154137ec319bf2017c24830a
Sherrina <robinsonsherrina@gmail.com>
2025-08-31 16:50:54 -0400

    updated room info with logo

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import Image from "next/image";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
 
 interface InfoProps {
     boardId: string;
-}
+};
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"],
+});
 
 export const Info = ({
     boardId,
@@ -16,12 +27,25 @@ export const Info = ({
     });
 
     if (!data) return <InfoSkeleton />;
-    
+
     return (
         <div 
             className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md"
         >
-            TODO: Information about the board
+            <Button variant="board" className="px-2">
+                <Image 
+                    src="/logo.svg"
+                    alt="Board logo"
+                    height={40}
+                    width={40}
+                />
+                <span className={cn(
+                    "font-semibold text-xl ml-2 text-black",
+                    font.className,
+                 )}>
+                    Blocs
+                </span>
+            </Button>
         </div>
     );
 };
