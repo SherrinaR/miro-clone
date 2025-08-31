183510e4c47ffdeec9fd708d13adc9c0989c22a0
Sherrina <robinsonsherrina@gmail.com>
2025-08-31 19:08:07 -0400

    added toolbar button to room toolbar block

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,19 +1,15 @@
+import { MousePointer2 } from "lucide-react";
+import { ToolButton } from "./tool-button";
+
 export  const Toolbar = () => {
     return (
         <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                <div>
-                    Pencil
-                </div>
-                <div>
-                    Square
-                </div>
-                <div>
-                    Circle
-                </div>
-                <div>
-                    Ellipsis
-                </div>
+                <ToolButton 
+                    label="Select"
+                    icon={MousePointer2}
+                    onClick={() => {}}
+                />
             </div>
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
                 <div>
