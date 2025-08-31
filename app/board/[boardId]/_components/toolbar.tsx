569d208930a585a120d1850765ed0ef2bd878d7b
Sherrina <robinsonsherrina@gmail.com>
2025-08-31 19:16:46 -0400

    added lucid icons to each toolbar button

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,4 +1,14 @@
-import { MousePointer2 } from "lucide-react";
+import { 
+    Circle, 
+    MousePointer2, 
+    Pencil, 
+    Redo2, 
+    Square, 
+    StickyNote, 
+    Type, 
+    Undo2 
+} from "lucide-react";
+
 import { ToolButton } from "./tool-button";
 
 export  const Toolbar = () => {
@@ -9,15 +19,52 @@ export  const Toolbar = () => {
                     label="Select"
                     icon={MousePointer2}
                     onClick={() => {}}
+                    isActive={false}
+                />
+                <ToolButton 
+                    label="Text"
+                    icon={Type}
+                    onClick={() => {}}
+                    isActive={false}
+                />
+                <ToolButton 
+                    label="Sticky note"
+                    icon={StickyNote}
+                    onClick={() => {}}
+                    isActive={false}
+                />
+                <ToolButton 
+                    label="Rectangle"
+                    icon={Square}
+                    onClick={() => {}}
+                    isActive={false}
+                />
+                <ToolButton 
+                    label="Ellipse"
+                    icon={Circle}
+                    onClick={() => {}}
+                    isActive={false}
+                />
+                <ToolButton 
+                    label="Pen"
+                    icon={Pencil}
+                    onClick={() => {}}
+                    isActive={false}
                 />
             </div>
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                <div>
-                    Undo
-                </div>
-                <div>
-                    Redo
-                </div>
+                <ToolButton 
+                    label="Undo"
+                    icon={Undo2}
+                    onClick={() => {}}
+                    isDisabled={true}
+                />
+                <ToolButton 
+                    label="Redo"
+                    icon={Redo2}
+                    onClick={() => {}}
+                    isDisabled={true}
+                />
             </div>
         </div>
     );
