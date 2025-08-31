460ea22769a2a573d3c66ef5c74171f51daaa88f
Sherrina <robinsonsherrina@gmail.com>
2025-08-31 16:18:00 -0400

    updated room skeleton

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -1,4 +1,22 @@
-export const Info = () => {
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface InfoProps {
+    boardId: string;
+}
+
+export const Info = ({
+    boardId,
+}: InfoProps) => {
+    const data = useQuery(api.board.get, {
+        id: boardId as Id<"boards">,
+    });
+
+    if (!data) return <InfoSkeleton />;
+    
     return (
         <div 
             className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md"
@@ -8,7 +26,7 @@ export const Info = () => {
     );
 };
 
-Info.Skeleton = function InfoSkeleton() {
+export const InfoSkeleton = () => {
     return (
         <div 
             className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md w-[300px]"
