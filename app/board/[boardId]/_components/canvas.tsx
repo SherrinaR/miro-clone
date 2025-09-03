3b5530ac8bca9cfbf7f8d6c1104989dd7c69f158
Sherrina <robinsonsherrina@gmail.com>
2025-09-02 21:31:28 -0400

    added cursor movement

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useState } from "react";
-import { CanvasMode, CanvasState } from "@/types/canvas";
+import React, { useCallback, useState } from "react";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
 
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useCanRedo, useCanUndo, useHistory,  } from "@/liveblocks.config";
+import { useCanRedo, useCanUndo, useHistory, useMutation,  } from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 
 interface CanvasProps {
@@ -18,9 +20,31 @@ export const Canvas = ({boardId,}: CanvasProps) => {
         mode: CanvasMode.None,
     });
 
+    const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    /* Change camera direction for infinite space */
+    const onWheel = useCallback((e: React.WheelEvent) => {
+
+        setCamera((camera) => ({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY,
+        }));
+    }, []);
+
+    const onPointerMove = useMutation((
+        { setMyPresence }, 
+        e: React.PointerEvent
+    ) => {
+        e.preventDefault();
+
+        const current = pointerEventToCanvasPoint(e, camera);
+
+        setMyPresence({ cursor: current });
+    }, []);
 
     return (
         <main
@@ -36,6 +60,15 @@ export const Canvas = ({boardId,}: CanvasProps) => {
                 undo={history.undo}
                 redo={history.redo}
             />
+            <svg 
+                className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+            >
+                <g>
+                    <CursorsPresence />
+                </g>
+            </svg>
         </main>
     );
 };
