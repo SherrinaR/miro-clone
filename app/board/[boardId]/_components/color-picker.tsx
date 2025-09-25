b34837d2623f3379e7a5290535a2b76a341e856a
Sherrina <robinsonsherrina@gmail.com>
2025-09-25 15:04:05 -0400

    added button for color picker

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { colorToCss } from "@/lib/utils";
 import { Color } from "@/types/canvas";
 
 interface ColorPickerProps {
@@ -10,8 +11,40 @@ export const ColorPicker = ({
     onChange,
 }: ColorPickerProps) => {
     return (
-        <div>
-            Color Picker!
+        <div
+            className="flex flex-wrap gap-2 items-center max-w-[164px] pr-2 mr-2 border-r border-neutral-200"
+        >
+            <ColorButton 
+                color={{
+                    r: 243,
+                    g: 82,
+                    b: 35
+                }}
+                onClick={onChange}
+            />
         </div>
     )
 };
+
+interface ColorButtonProps {
+    onClick: (color: Color) => void;
+    color: Color;
+};
+
+const ColorButton = ({
+    onClick,
+    color,
+}: ColorButtonProps) => {
+    return (
+        <button 
+            className="w-8 h-8 items-center flex justify-center hover:opacity-75 transition"
+            onClick={() => onClick(color)}
+        >
+            <div 
+                className="h-8 w-8 rounded-md border border-neutral-300"
+                style={{ background: colorToCss(color) }}
+            />
+
+        </button>
+    )
+}
