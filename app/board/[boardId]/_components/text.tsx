436a68cc5fb0e1de559582f40d57aaae66ff42a0
Sherrina <robinsonsherrina@gmail.com>
2025-09-29 19:35:33 -0400

    added texts stylings and method to synchronize texts

--- app/board/[boardId]/_components/text.tsx
+++ app/board/[boardId]/_components/text.tsx
@@ -10,6 +10,20 @@ const font = Kalam({
     weight: ["400"],
 });
 
+const calculateFontSize = (width: number, height: number) => {
+    const maxFontSize = 96;
+    const scaleFactor = 0.5;
+    const fontSizeBasedOnHeight = height * scaleFactor;
+    const fontSizeBasedOnWidth = width * scaleFactor;
+
+    return Math.min(
+        fontSizeBasedOnHeight,
+        fontSizeBasedOnWidth,
+        maxFontSize
+    );
+}
+    
+
 interface TextProps {
     id: string;
     layer: TextLayer;
@@ -25,6 +39,20 @@ export const Text = ({
 }: TextProps) => {
     const { x, y, width, height, fill, value } = layer;
 
+    /* Medthod to synchronize texts */
+    const updateValue = useMutation((
+        { storage },
+        newValue: string,
+    ) => {
+        const liveLayers = storage.get("layers");
+
+        liveLayers.get(id)?.set("value", newValue);
+    }, []);
+
+    const handleContentChange = (e: ContentEditableEvent) => {
+        updateValue(e.target.value);
+    };
+
     return (
         <foreignObject
             x={x}
@@ -37,10 +65,13 @@ export const Text = ({
             }}
         >
             <ContentEditable 
-                html={"Text"}
-                onChange={() => {}}
+                html={value || "Text"}
+                onChange={handleContentChange}
                 className={cn("h-full w-full flex items-center justify-center text-center drop-shadow-md outline-none", font.className)}
-                style={{color: fill ? colorToCss(fill) : "#000",}}
+                style={{
+                    fontSize: calculateFontSize(width, height),
+                    color: fill ? colorToCss(fill) : "#000",
+                }}
             />
         </foreignObject>
     )
